6d4e41387493dc75a5bca95bd7a451f9cee8cea2
Trương Thị Ngọc Phượng <121546890+phuongttn@users.noreply.github.com>
2026-04-05 14:02:45 +0700

    Update fetch_demo_data.sh

--- fetch_demo_data.sh
+++ fetch_demo_data.sh
@@ -1,8 +1,4 @@
 pip install gdown
 cd "$(dirname "$0")"
-gdown --folder "https://drive.google.com/drive/folders/1-S8oZa3TWx41bblIub32L6aqkgp3p3oj?usp=sharing"
+gdown --folder "https://drive.google.com/drive/folders/1VkYGz1a0DX_3gfs56LqbcDzaUGWA4rO7?usp=sharing"
 
-if [ -d "rants_demo_data/_DATA" ]; then
-    rm -rf ./_DATA
-    mv rants_demo_data/_DATA ./_DATA
-fi
